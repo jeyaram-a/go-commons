db406d39fce1a0c3e08496b01defc3e6a835a420
jeyaram <jeyarambe2018@gmail.com>
2023-06-20 11:59:43 +0530

    adds transformer (#1)
    Also refactors + test cases for router

--- router.go
+++ router.go
@@ -4,15 +4,42 @@ import (
 	"sync"
 )
 
+// Hashable. implement Hash()
+// Usage:
+// 		type Integer int
+// 		func (x Integer) Hash() uint32 {
+// 			return uint32(x % 5)
+// 		}
+
+type Hashable interface {
+	Hash() uint32
+}
+
+// Router routes Hashable input to a route.
+// Parallelism is the number of unique routes.
+// Each of these unique routes can be consumed
+// parallely
+//
+// Route logic:
+//
+//	route = Hashable.hash() % parallelism
+//
+// Usage:
+//
+//	router := NewRouter(uint32(2))
+//	router.Subscribe(func(in Hashable) {
+//		fmt.Printf("%v", in)
+//	} )
+//	for i:=0; i<10; i++ {
+//		router.Route(Integer(i))
+//	}
+//	router.Close()
 type Router[T Hashable] struct {
 	subscriptionIndex int
 	parallelism       uint32
 	routes            []chan T
 	wg                *sync.WaitGroup
-}
-
-type Hashable interface {
-	Hash() uint32
+	closed            bool
 }
 
 func NewRouter[T Hashable](parallelism uint32) *Router[T] {
@@ -30,11 +57,17 @@ func NewRouter[T Hashable](parallelism uint32) *Router[T] {
 	}
 }
 
+// Routes the input to the designated route
 func (router *Router[T]) Route(in T) {
+	if router.closed {
+		panic("sending to a closed router")
+	}
 	index := in.Hash() % router.parallelism
 	router.routes[index] <- in
 }
 
+// Subscribe a callback for one of the routes.
+// Routes are subscribed in round-robin fashion
 func (router *Router[T]) Subscribe(callback func(in T)) {
 	if router.subscriptionIndex == int(router.parallelism) {
 		router.subscriptionIndex = 0
@@ -50,7 +83,12 @@ func (router *Router[T]) Subscribe(callback func(in T)) {
 	router.subscriptionIndex += 1
 }
 
+// Closes all routes. Also waits for all subscriptions to end
 func (router *Router[T]) Close() {
+	if router.closed {
+		panic("closing an already closed router")
+	}
+	router.closed = true
 	for _, route := range router.routes {
 		close(route)
 	}
